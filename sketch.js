a51c9e3282fd2406ac457e5ba74acec14e447714
sladerph <herduinpierre@yahoo.com>
2020-12-31 04:35:49 +0100

    Add automatization

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let debug_logs = false
 let debug_attribute = 'none' // 'none' : 'value' : 'index'
 let debug_montecarlo = false
 
-let speed = .1
+let speed = 1
 let max_player = 2
-let players = ['random', 'montecarlo']
+let players = ['bruteforce', 'montecarlo']
 let player = 0
 let game_size = 600
 
@@ -36,14 +36,28 @@ function play() {
         return
     } else if (current === 'montecarlo') {
         monteCarloPlay()
-    } else if(current === 'random') {
+    } else if (current === 'random') {
         m.playRandomValidAtomic()
+    } else if (current === 'bruteforce') {
+        if (!bruteForce()) {
+            m.playRandomValidAtomic()
+        }
+    } else if ('montecarlodumb') {
+        thinking_time /= 2
+        monteCarloPlay()
+        thinking_time *= 2
     } else {
         throw new Error('Unrecognized player type: ' + current)
     }
 
     nextPlayer()
 }
+
+
+var nb_wins = 0
+var nb_losses = 0
+var nb_draws = 0
+var nb_played = 0
 
 function nextPlayer() {
     player++
@@ -56,6 +70,23 @@ function nextPlayer() {
     redraw()
 
     if(!m.gameOver) {
+        setTimeout(play, (players[player] === 'bruteforce' || players[player] === 'random') ? (speed < 1 ? speed * 10e3 : 0 ) : 0)
+    } else {
+        const val = m.value
+        m.initialize()
+        t = undefined
+        nb_played++
+
+        if (val === DRAW) nb_draws++
+        else if (val === 1) nb_losses++
+        else nb_wins++
+
+        console.log("### End of game ###")
+        console.log("Total played : " + nb_played)
+        console.log("Total wined  : " + nb_wins)
+        console.log("Total lossed : " + nb_losses)
+        console.log("Total drawed : " + nb_draws)
+
         setTimeout(play, players[player] === 'random' ? (speed < 1 ? speed * 10e3 : 0 ) : 0)
     }
 }
@@ -109,14 +140,15 @@ function graphClicked() {
 }
 
 function monteCarloPlay() {
-    console.log("Start monte carlo")
+    if (players[0] === "human")
+        console.log("Start monte carlo")
     if (t === undefined)
         t = new Tree(_.cloneDeep(m))
     else {
         if (t.optimize(_.cloneDeep(m)) === false) {
             console.log("PROBLEME")
         }
-        console.log(t.root.trials)
+        // console.log(t.root.trials)
     }
     let start = millis()
 
@@ -128,9 +160,26 @@ function monteCarloPlay() {
 
     let best = t.getMostVisitedNode()
 
-    console.log(best)
+    // console.log(best)
 
     console.log('calculated ' + t.root.trials + ' games, next move has ' + best.losses + '/' + best.trials + ' win outcomes ' + 
-                '(' + nf((best.losses)/best.trials*100,0,2) + '%)', best.draws/best.trials*100)
+                '(' + nf((best.losses)/best.trials*100,0,2) + '%)')
     m.playPath(best.move)
+}
+
+function bruteForce() {
+    // const valid_zone = m.getChild(m.nextZone)
+    const valid_atomics = m.getPlayableAtomics()
+
+    for(let elm of valid_atomics) {
+        const atomic = _.cloneDeep(elm)
+
+        if(atomic.play()) {
+            m.playPath(atomic.getPathArray())
+            // console.log('forced win on ' + atomic.getPath())
+            return true
+        }
+    }
+
+    return false
 }
